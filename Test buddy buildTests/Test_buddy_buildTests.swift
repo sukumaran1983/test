cafe3833e05100ff543a4ecd5161d1eb7f9494bc
suku <suku@apple.com>
2018-05-15 11:37:29 -0700

    Initial Commit

---
+++ Test buddy buildTests/Test_buddy_buildTests.swift
@@ -0,0 +1,36 @@
+//
+//  Test_buddy_buildTests.swift
+//  Test buddy buildTests
+//
+//  Created by Sukumaran Raghavan on 5/15/18.
+//  Copyright © 2018 Suku Test. All rights reserved.
+//
+
+import XCTest
+@testable import Test_buddy_build
+
+class Test_buddy_buildTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
